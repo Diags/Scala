2977ac7cd21c2fc2b741d8be587c224dbd5ef635
dsylla <dsylla@pc-dev-107.agixis.local>
2018-01-17 15:16:49 +0100

    ghg

--- ScalaSpark/src/main/scala/com/scalspark/TweetCount.scala
+++ ScalaSpark/src/main/scala/com/scalspark/TweetCount.scala
@@ -2,6 +2,21 @@
 
 package com.scalspark
 
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
 object TweetCount {
-  
+  def main(args: Array[String]) {
+    val conf = new SparkConf()
+    val context = new SparkContext(conf)
+    val fileText = context.textFile("/tweetFile")
+    val splitsTab = fileText.flatMap(line => line.split(" "))
+    val counts = splitsTab.map(word => (word, 1))
+    val wordCount = counts.reduceByKey(_ + _)
+    val wordcount_sorted = wordCount.sortByKey()
+    wordcount_sorted.foreach(println)
+    //store output into a file system
+    wordcount_sorted.saveAsTextFile("bouna")
+
+  }
 }
