66d7cc030aee27b757b3aa9131767e538498d0e2
dsylla <dsylla@pc-dev-107.agixis.local>
2018-01-17 15:17:10 +0100

    test

---
+++ ScalaSpark/src/main/scala/MainTweet.scala
@@ -0,0 +1,19 @@
+
+import org.apache.spark._
+
+
+object MainTweet {
+    def main(args: Array[String]) {
+    val conf = new SparkConf()
+    val context = new SparkContext(conf)
+    val fileText = context.textFile("/tweetFile")
+    val splitsTab = fileText.flatMap(line => line.split(" "))
+    val counts = splitsTab.map(word => (word, 1))
+    val wordCount = counts.reduceByKey(_ + _)
+    val wordcount_sorted = wordCount.sortByKey()
+    wordcount_sorted.foreach(println)
+    //store output into a file system
+    wordcount_sorted.saveAsTextFile("bouna")
+
+  }
+}
